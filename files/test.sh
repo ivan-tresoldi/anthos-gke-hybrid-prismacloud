ef14ff6649f28b3dd9d17fd9063ead6a279d672c
ivan-tresoldi <67233434+ivan-tresoldi@users.noreply.github.com>
2021-09-22 15:52:00 +0200

    -

--- files/test.sh
+++ files/test.sh
@@ -1,2 +1,10 @@
+./twistcli coderepo scan --address $TL_CONSOLE --details -u $TL_USER -p $TL_PASS "/workspace/$LOCAL_REPO_DIR/app"
 result=$(curl -k -u $TL_USER:$TL_PASS -H 'Content-Type: application/json' "$TL_CONSOLE/api/v1/coderepos-ci?limit=1&reverse=true&sort=scanTime"|jq '.[0].pass')
-echo $result
+
+if [ "$result" = "true" ] || [ "$BYPASS_REPOSCAN" = "1" ]; then
+   echo "Code Repo scan passed!"
+   exit 0
+else
+   echo "Code Repo scan failed!"
+   exit 1
+fi
