ffa282078d083ff87dfda0f32ab8603b3ff7a631
ivan-tresoldi <67233434+ivan-tresoldi@users.noreply.github.com>
2021-09-22 15:43:43 +0200

    -

--- files/test.sh
+++ files/test.sh
@@ -1,5 +1,2 @@
-echo "This is a test"
-echo $TL_CONSOLE
-echo $TL_USER
-echo $TL_PASS
-curl https://www.google.com
+result=$(curl -k -u $TL_USER:$TL_PASS -H 'Content-Type: application/json' "$TL_CONSOLE/api/v1/coderepos-ci?limit=1&reverse=true&sort=scanTime"|jq '.[0].pass')
+echo $result
