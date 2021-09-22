9d52856a12e9aeb012a8b3a570dbf1a9ccd5734c
ivan-tresoldi <67233434+ivan-tresoldi@users.noreply.github.com>
2021-09-22 11:52:39 +0200

    -

--- files/repoScan.sh
+++ files/repoScan.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 
-./twistcli coderepo scan --address $TL_CONSOLE --details -u $TL_USER -p $TL_PASS /workspace/anthos-gke-hybrid-prismacloud/app"
+./twistcli coderepo scan --address $TL_CONSOLE --details -u $TL_USER -p $TL_PASS "/workspace/anthos-gke-hybrid-prismacloud/app"
 result=$("curl -k -u $TL_USER:$TL_PASS -H 'Content-Type: application/json' "$TL_CONSOLE/api/v1/coderepos-ci?limit=1&reverse=true&sort=scanTime""|jq '.[0].pass')
 
 echo "$TL_CONSOLE"
