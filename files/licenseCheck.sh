7483f8fb45a82af29f6b41a58784e081c47604b6
ivan-tresoldi <67233434+ivan-tresoldi@users.noreply.github.com>
2021-09-22 18:58:30 +0200

    -

--- files/licenseCheck.sh
+++ files/licenseCheck.sh
@@ -2,13 +2,13 @@ if [ "$1" = 1 ]; then
     echo "OSS License check bypassed by User"
     exit 0
 else
-   ./twistcli coderepo scan --address $TL_CONSOLE -u $TL_USER -p $TL_PASS "/workspace/license" 
-   result=$(curl -k -u $TL_USER:$TL_PASS -H 'Content-Type: application/json' "$TL_CONSOLE/api/v1/coderepos-ci?limit=1&reverse=true&sort=scanTime"|jq '.[0].pass')
-   if [ "$result" = "true" ] then
-      echo "License check passed!"
-      exit 0
-   else
-      echo "License check failed!"
-      exit 1
-   fi
+    ./twistcli coderepo scan --address $TL_CONSOLE --details -u $TL_USER -p $TL_PASS "/workspace/app"
+    result=$(curl -k -u $TL_USER:$TL_PASS -H 'Content-Type: application/json' "$TL_CONSOLE/api/v1/coderepos-ci?limit=1&reverse=true&sort=scanTime"|jq '.[0].pass')
+    if [ "$result" = "true" ]; then
+        echo "License check passed!"
+        exit 0
+    else
+        echo "License check failed!"
+        exit 1
+    fi
 fi
