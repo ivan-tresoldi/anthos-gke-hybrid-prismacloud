b2434c4ed8ab4c55fe8b5a6bafbbcbac22048c75
ivan-tresoldi <67233434+ivan-tresoldi@users.noreply.github.com>
2021-09-22 10:01:47 +0200

    -

--- files/repoScan.sh
+++ files/repoScan.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 
-./twistcli coderepo scan --address $TL_CONSOLE --details -u $TL_USER -p $TL_PASS $LOCAL_REPO_DIR/app
-result=$(curl -k -u $TL_USER:$TL_PASS -H 'Content-Type: application/json' "$TL_CONSOLE/api/v1/coderepos-ci?limit=1&reverse=true&sort=scanTime"|jq '.[0].pass')
+./twistcli coderepo scan --address $TL_CONSOLE --details -u $TL_USER -p $TL_PASS "$LOCAL_REPO_DIR/app"
+result=$(./curl -k -u $TL_USER:$TL_PASS -H 'Content-Type: application/json' "$TL_CONSOLE/api/v1/coderepos-ci?limit=1&reverse=true&sort=scanTime"|jq '.[0].pass')
 
 echo "$TL_CONSOLE"
 echo "$REPO_DIR"
