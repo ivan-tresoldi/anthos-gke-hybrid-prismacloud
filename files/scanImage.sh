a43b7b3fdf4bb8e21d38af1a9ee9df1207899f58
ivan-tresoldi <67233434+ivan-tresoldi@users.noreply.github.com>
2021-09-23 16:52:01 +0200

    -

--- files/scanImage.sh
+++ files/scanImage.sh
@@ -2,8 +2,7 @@ if [ "$1" = 1 ]; then
     echo "Image Scan bypassed by User"
     exit 0
 else
-   docker pull itresoldi/evilpetclinic:latest     
-   ./twistcli images scan --u $TL_USER --p $TL_PASS --address $TL_CONSOLE --containerized --details itresoldi/evilpetclinic -o output.json
+   ./twistcli images scan --u $TL_USER --p $TL_PASS --address $TL_CONSOLE --details itresoldi/evilpetclinic -o output.json
    result=$(curl -k -u $TL_USER:$TL_PASS -H 'Content-Type: application/json' "$TL_CONSOLE/api/v1/scans?type=ciImage&collections=evilpetclinic&reverse=true&sort=time&limit=1"|jq '.[0].vulnFailureSummary'|grep "Scan failed")
    if [ -z "$result" ]; then
       echo "Image Vulnerability scan passed!"
