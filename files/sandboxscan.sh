ccd86f081f4b6c8b9b4502a723a9550ff035d26b
ivan-tresoldi <67233434+ivan-tresoldi@users.noreply.github.com>
2021-09-28 15:54:42 +0200

    -

--- files/sandboxscan.sh
+++ files/sandboxscan.sh
@@ -2,8 +2,9 @@ if [ "$BYPASS_SANDBOX" = 1 ]; then
     echo "Image Sandbox Scan bypassed by User"
     exit 0
 else
-   ./twistcli sandbox -u $TL_USER -p $TL_PASS --address $TL_CONSOLE --output-file "sandbox.json" --analysis-duration 1m itresoldi/evilpetclinic:latest
-   
+   # ./twistcli sandbox -u $TL_USER -p $TL_PASS --address $TL_CONSOLE --output-file "sandbox.json" --analysis-duration 1m itresoldi/evilpetclinic:latest
+   echo "Scanning Image..."
+
    status=$?
    echo "Exit status is:"
    echo $status
