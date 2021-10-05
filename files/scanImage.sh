9d243004fbe3ae530974c812a4f5cf6d9be25228
ivan-tresoldi <67233434+ivan-tresoldi@users.noreply.github.com>
2021-10-05 11:58:25 +0200

    -

--- files/scanImage.sh
+++ files/scanImage.sh
@@ -2,7 +2,7 @@ if [ "$BYPASS_IMAGESCAN" = 1 ]; then
     echo "Image Scan bypassed by User"
     exit 0
 else
-   ./twistcli images scan --u $TL_USER --p $TL_PASS --address $TL_CONSOLE --details gcr.io/pcc-dev-sandbox/evilpetclinic -o output.json
+   ./twistcli images scan --u $TL_USER --p $TL_PASS --address $TL_CONSOLE --details gcr.io/pcc-dev-sandbox/evilpetclinic:latest -o output.json
    result=$(curl -k -u $TL_USER:$TL_PASS -H 'Content-Type: application/json' "$TL_CONSOLE/api/v1/scans?type=ciImage&collections=evilpetclinic&reverse=true&sort=time&limit=1"|jq '.[0].vulnFailureSummary'|grep "Scan failed")
    if [ -z "$result" ]; then
       echo "Image Vulnerability scan passed!"
